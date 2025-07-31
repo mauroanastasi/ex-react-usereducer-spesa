b12d32e01c545f1e5953faf3abc144fbfd3decf8
mauroanastasi <mauroanastasi1@gmail.com>
2025-07-31 12:33:00 +0200

    [UPD] addToCart with quantity 1

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useState } from 'react';
 
 const ProductList = () => {
 
+    const [addedProducts, setAddedProducts] = useState([])
+
+    console.log(addedProducts)
     const products = [
         { name: 'Mela', price: 0.5 },
         { name: 'Pane', price: 1.2 },
@@ -9,11 +13,23 @@ const ProductList = () => {
         { name: 'Pasta', price: 0.7 },
     ];
 
+    function addToCart(product) {
+        let esiste = addedProducts.some(p =>
+            p.name === product.name
+        )
+        if (esiste) {
+            return
+        } setAddedProducts(now => [...now, { ...product, quantity: 1 }]
+        )
+    }
+
+
     return (
         <div>
-            {products.map((p) => (
+            {products.map((p, i) => (
                 <ul>
-                    <li>{p.name} - {p.price}</li>
+                    <li key={i}> {p.name} - {p.price.toFixed(2)}  <button onClick={() => addToCart(p)} >Aggiungi al Carrello</button>
+                    </li>
                 </ul>
             ))}
         </div>
